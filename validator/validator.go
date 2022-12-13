977e1de71d1765db763cf09264159cf4133c9ca2
Matt Massoodi <matt@puroxy.io>
2022-12-12 21:25:22 -0800

    catch validator errs

--- validator/validator.go
+++ validator/validator.go
@@ -5,6 +5,10 @@ import (
 	val "github.com/go-playground/validator"
 )
 
+var defaultValidators = map[string]val.Func{
+	"oid": validators.ValidateObjectID,
+}
+
 type Validator struct {
 	validate *val.Validate
 }
@@ -13,13 +17,20 @@ func (v *Validator) ValidateStruct(o interface{}) error {
 	return v.validate.Struct(o)
 }
 
-func (v *Validator) AddValidator(key string, fn val.Func) {
-	v.validate.RegisterValidation(key, fn)
+func (v *Validator) AddValidator(key string, fn val.Func) error {
+	if err := v.validate.RegisterValidation(key, fn); err != nil {
+		return err
+	}
+	return nil
 }
 
-func New() *Validator {
+func New() (*Validator, error) {
 	v := val.New()
 	validator := &Validator{validate: v}
-	validator.AddValidator("oid", validators.ValidateObjectID)
-	return validator
+	for key, fn := range defaultValidators {
+		if err := validator.AddValidator(key, fn); err != nil {
+			return nil, err
+		}
+	}
+	return validator, nil
 }
